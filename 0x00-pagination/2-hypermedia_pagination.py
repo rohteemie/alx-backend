8aeeb1c3b12f585e1f3263ff29490afa093edcf5
Rotimi Adebowale Owolabi <iamrotimiowolabi@gmail.com>
2023-02-20 10:29:36 +0100

    function docstring updated

--- 0x00-pagination/2-hypermedia_pagination.py
+++ 0x00-pagination/2-hypermedia_pagination.py
@@ -1,3 +1,17 @@
+#!/usr/bin/env python3
+''' Description: Implement a get_hyper method that takes the same arguments
+                 (and defaults) as get_page and returns a dictionary containing
+                 the following key-value pairs:
+        - page_size: the length of the returned dataset page
+        - page: the current page number
+        - data: the dataset page (equivalent to return from previous task)
+        - next_page: number of the next page, None if no next page
+        - prev_page: number of the previous page, None if no previous page
+        -total_pages: the total number of pages in the dataset as an integer
+    Make sure to reuse get_page in your implementation.
+    You can use the math module if necessary.
+'''
+
 import csv
 from math import ceil
 from typing import List
