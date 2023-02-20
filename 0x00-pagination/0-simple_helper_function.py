de593575ae3d5e2aeacda8567e4d304d66cc2558
Rotimi Adebowale Owolabi <iamrotimiowolabi@gmail.com>
2023-02-20 09:30:30 +0100

    README updated

--- 0x00-pagination/0-simple_helper_function.py
+++ 0x00-pagination/0-simple_helper_function.py
@@ -10,13 +10,13 @@ from typing import Tuple
 '''
 
 def index_range(page: int, page_size: int) -> Tuple[int, int]:
-  """_summary_
+    """_summary_
 
-  Args:
-      page (int): _Current Page_
-      page_size (int): _Numbers of items on per page _
+    Args:
+        page (int): _Current Page_
+        page_size (int): _Numbers of items on per page _
 
-  Returns:
-      Tuple[int, int]: _Current items, The end of items_
-  """
+    Returns:
+        Tuple[int, int]: _Current items, The end of items_
+    """
     return ((page_size * (page - 1)), page_size * page)
